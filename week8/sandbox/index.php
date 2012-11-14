98caf2fadc5f2cd111a3f528dce35a2799c52902
Vikram Padval <vikrampadval@yahoo.com>
2012-11-14 15:37:40 -0800

    end week8

--- week8/sandbox/index.php
+++ week8/sandbox/index.php
@@ -1,3 +1,12 @@
+<?php
+include 'Mobile_Detect.php';
+$detect = new Mobile_Detect();
+
+if ($detect->isMobile()) {
+    // Any mobile device.
+    header("Location: mobiletest/iphone.php?url=stanford.edu/~vpadval/cgi-bin/week8/sandbox/mobile.php");
+}
+?>
 <!DOCTYPE html> 
 <html>
 
